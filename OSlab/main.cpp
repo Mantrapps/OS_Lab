787d709dcfc1e8fc96ec4122be4d969c4428e9d2
Kaiz <kevin.zhu.cn@outlook.com>
2016-02-01 17:36:18 -0600

    Initial commit from local git

--- OSlab/main.cpp
+++ OSlab/main.cpp
@@ -7,9 +7,41 @@
 //
 
 #include <iostream>
+#include <unistd.h>
 
 int main(int argc, const char * argv[]) {
     // insert code here...
-    std::cout << "Hello, World!\n";
+    printf("--beginning of program\n");
+    
+    int counter = 0;
+    pid_t pid = fork();
+    
+    if (pid == 0)
+    {
+        // child process
+        int i = 0;
+        for (; i < 5; ++i)
+        {
+            printf("child process: counter=%d\n", ++counter);
+        }
+    }
+    else if (pid > 0)
+    {
+        // parent process
+        int j = 0;
+        for (; j < 5; ++j)
+        {
+            printf("parent process: counter=%d\n", ++counter);
+        }
+    }
+    else
+    {
+        // fork failed
+        printf("fork() failed!\n");
+        return 1;
+    }
+    
+    printf("--end of program--\n");
+    
     return 0;
 }
